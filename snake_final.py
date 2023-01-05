f70f5fd2557a7ab931fc353c91e1bd76b7a9df55
Augusto Cesar <85577633+augustocrp@users.noreply.github.com>
2023-01-05 20:58:57 -0300

    Update snake_final.py

--- snake_final.py
+++ snake_final.py
@@ -116,5 +116,4 @@ while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
-                exit()
-                
+                exit()                
